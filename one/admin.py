a0dc6e101acad20ea56e7c06368c77894c817453
Ankur Jain <ankurjain8448@gmail.com>
2017-08-29 16:03:10 +0530

    some model creation

--- one/admin.py
+++ one/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
-
+from one.models import *
 # Register your models here.
+
+class DepartmentAdmin(admin.ModelAdmin):
+    pass
+
+class EmployeeAdmin(admin.ModelAdmin):
+    pass
+
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+class PublisherAdmin(admin.ModelAdmin):
+    pass
+
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+class StoreAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Author, AuthorAdmin)    
+admin.site.register(Publisher, PublisherAdmin)
+
+admin.site.register(Book, BookAdmin)    
+admin.site.register(Store, StoreAdmin)
+
+admin.site.register(Department, DepartmentAdmin)    
+admin.site.register(Employee, EmployeeAdmin)
--- one/models.py
+++ one/models.py
@@ -4,9 +4,43 @@ from django.db import models
 
 # Create your models here.
 
+class Department(models.Model):
+	name = models.CharField(max_length = 255, blank = False)
+
+	def __str__(self):
+		return self.name
+
+
 class Employee(models.Model):
-	pass
+	name = models.CharField(max_length = 255, blank = False)
+	dept = models.ForeignKey(Department)
+
+	def __str__(self):
+		return self.name + " -->> " + self.dept.name
 
 
-class Department(object):
-	pass
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=300)
+    num_awards = models.IntegerField()
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=300)
+    pages = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    rating = models.FloatField()
+    authors = models.ManyToManyField(Author)
+    publisher = models.ForeignKey(Publisher)
+    pubdate = models.DateField()
+
+
+class Store(models.Model):
+    name = models.CharField(max_length=300)
+    books = models.ManyToManyField(Book)
+    registered_users = models.PositiveIntegerField()
+
